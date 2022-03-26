7b5effd9ca8718bbbaeb0c83cb091c7ca25b5cb4
yevhen <zheka.chetverikov.322@gmail.com>
2022-03-27 00:45:58 +0200

    feat: add routes add some design

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
+@Module({
+  imports: [
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.fyqrj.mongodb.net/music-platform',
+    ),
+    FileModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
+  ],
+})
+export class AppModule {}
---
+++ src/track/schemas/comment.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Track } from './track.schema';
+import mongoose from 'mongoose';
+export type CommentDocument = Comment & Document;
+
+@Schema()
+export class Comment {
+  @Prop()
+  username: string;
+
+  @Prop()
+  text: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId })
+  track: Track;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
